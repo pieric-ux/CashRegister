40438ea6c6e4f3abe6332df6791f9d2fd6fa942a
pieric-ux <93613004+pieric-ux@users.noreply.github.com>
2023-06-16 23:59:29 +0200

    cr_apps nullable

--- database/migrations/2023_03_31_124206_create_cr_apps_table.php
+++ database/migrations/2023_03_31_124206_create_cr_apps_table.php
@@ -15,11 +15,11 @@ return new class extends Migration
             $table->id();
             $table->string('name')->unique();
             $table->string('slug');
-            $table->string('desciption');
-            $table->date('start_date');
-            $table->date('end_date');
-            $table->string('location');
-            $table->string('website');
+            $table->string('desciption')->nullable();
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+            $table->string('location')->nullable();
+            $table->string('website')->nullable();
             $table->unsignedBigInteger('fk_customer_id');
             $table->foreign('fk_customer_id')->references('id')->on('customers')->onDelete('cascade');
             $table->timestamps();
