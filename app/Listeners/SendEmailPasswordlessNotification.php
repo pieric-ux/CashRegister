0f16e1990ef3c2c2f5a7bae2d86fd02eeac39a46
pieric-ux <93613004+pieric-ux@users.noreply.github.com>
2023-07-12 00:00:15 +0200

    PasswordlessNotification

---
+++ app/Listeners/SendEmailPasswordlessNotification.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Models\CR_Employees;
+use App\Notifications\EmployeePasswordlessNotification;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Notification;
+
+class SendEmailPasswordlessNotification
+{
+    /**
+     * Handle the event.
+     *
+     * @param  \Illuminate\Auth\Events\Registered  $event
+     * @return void
+     */
+    public function handle(Registered $event): void
+    {
+        if ($event->user instanceof CR_Employees) {
+            $loginRoute = 'employees.login';
+            $applicationSlug = $event->user->cr_workstations->cr_apps->slug;
+
+            Notification::send($event->user, new EmployeePasswordlessNotification($event->user->passwordless, $loginRoute, $applicationSlug));
+        }
+    }
+}
---
+++ app/Notifications/EmployeePasswordlessNotification.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Lang;
+
+class EmployeePasswordlessNotification extends Notification
+{
+    use Queueable;
+
+    protected $passwordless;
+    protected $loginRoute;
+    protected $applicationSlug;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($passwordless, $loginRoute, $applicationSlug)
+    {
+        $this->passwordless = $passwordless;
+        $this->loginRoute = $loginRoute;
+        $this->applicationSlug = $applicationSlug;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        $loginUrl = route($this->loginRoute, ['app' => $this->applicationSlug]);
+
+        return (new MailMessage)
+            ->subject(Lang::get('Activation code'))
+            ->line(Lang::get('Please use the following activation code: :code', ['code' => $this->passwordless]))
+            ->action(Lang::get('Login'), $loginUrl);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
