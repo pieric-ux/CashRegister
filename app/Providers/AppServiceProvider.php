e36b250ea64b2b4f010ff0089e0bd4145bd06b3c
pieric-ux <93613004+pieric-ux@users.noreply.github.com>
2023-08-22 15:37:09 +0200

    htaccess + password is_Production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,6 +27,14 @@ class AppServiceProvider extends ServiceProvider
                 'locale' => App::getLocale(),
                 'locales' => config('app.locales'),
             ];
+        });
+
+        Password::defaults(function () {
+            $rule = Password::min(8);
+
+            return $this->app->isProduction()
+                ? $rule->min(12)->mixedCase()->symbols()->uncompromised()
+                : $rule;
         });
     }
 }
