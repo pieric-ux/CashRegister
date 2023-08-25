82f27ffb87975a6683693949011313206c039bcc
pieric-ux <93613004+pieric-ux@users.noreply.github.com>
2023-08-25 23:41:34 +0200

    Header HTTP

--- app/Http/Middleware/AddContentSecurityPolicyHeaders.php
+++ app/Http/Middleware/AddContentSecurityPolicyHeaders.php
@@ -16,14 +16,10 @@ class AddContentSecurityPolicyHeaders
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $response = $next($request);
+        Vite::useCspNonce();
 
-        $cspNonce = Vite::useCspNonce();
-
-        $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'nonce-$cspNonce'; style-src 'self' 'nonce-$cspNonce'; font-src 'self' https://fonts.bunny.net; img-src 'self' data:; connect-src 'self'; frame-src 'self'; frame-ancestors 'none'; media-src 'self'; object-src 'self'; manifest-src 'self'; prefetch-src 'self'; form-action 'self';");
-        $response->headers->set('X-Frame-Options', 'SAMEORIGIN');
-        $response->headers->set('X-Content-Type-Options', 'nosniff');
-
-        return $response;
+        return $next($request)->withHeaders([
+            'Content-Security-Policy' => "default-src 'self'; script-src 'self' 'nonce-{{ Vite::cspNonce() }}'; style-src 'self' 'nonce-{{ Vite::cspNonce() }}'; font-src 'self' https://fonts.bunny.net; img-src 'self' data:;connect-src 'self'; frame-src 'self'; frame-ancestors 'none'; media-src 'self'; object-src 'self'; manifest-src 'self'; prefetch-src 'self'; form-action 'self';",
+        ]);
     }
 }
