d5fc7f5e32ba918370009cf02089113411092a4a
pieric-ux <93613004+pieric-ux@users.noreply.github.com>
2023-12-31 15:22:08 +0100

    refactor: Improve code structure and utilize clsx for class handling

--- resources/js/Pages/Customers/Application/Categories_Products/Index.jsx
+++ resources/js/Pages/Customers/Application/Categories_Products/Index.jsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { useState, useEffect } from 'react';
 import { Head } from '@inertiajs/react';
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
@@ -11,54 +12,27 @@ import { useTranslation } from 'react-i18next';
 export default function Index({ customerAuth, application, categories, localization }) {
     const { t } = useTranslation();
 
-    {
-        /* State to hold the updated categories */
-    }
     const [updatedCategories, setUpdatedCategories] = useState(categories);
 
-    {
-        /* Update the state when the 'categories' prop changes */
-    }
     useEffect(() => {
         setUpdatedCategories(categories);
     }, [categories]);
 
-    {
-        /* Callback for handling the end of a drag-and-drop operation */
-    }
     const onDragEnd = async (result) => {
-        {
-            /* Destructure the result to get the destination and source */
-        }
         const { destination, source } = result;
 
-        {
-            /* If there's no destination, return */
-        }
         if (!destination) {
             return;
         }
 
-        {
-            /* If the destination is the same as the source, return */
-        }
         if (destination.index === source.index) {
             return;
         }
 
-        {
-            /* Get the category being moved */
-        }
         const movedCategory = updatedCategories.splice(source.index, 1)[0];
 
-        {
-            /* Insert the moved category at the destination index */
-        }
         updatedCategories.splice(destination.index, 0, movedCategory);
 
-        {
-            /* Update the order of categories based on their new positions */
-        }
         const updatedCategoriesWithOrder = updatedCategories.map((category, index) => {
             return {
                 ...category,
@@ -66,17 +40,11 @@ export default function Index({ customerAuth, application, categories, localizat
             };
         });
 
-        {
-            /* Update the server using Axios */
-        }
         await axios
             .patch(route('categories.updateDragAndDrop'), {
                 categories: updatedCategoriesWithOrder,
             })
             .then(function (response) {
-                {
-                    /* Update the state with the response data */
-                }
                 setUpdatedCategories(response.data.categories);
             });
     };
@@ -89,7 +57,12 @@ export default function Index({ customerAuth, application, categories, localizat
         >
             <Head title={application.name} />
             <div className='mx-auto max-w-7xl space-y-6 px-2 sm:px-6 lg:px-8'>
-                <div className='rounded-lg bg-white p-4 shadow-md transition duration-300 ease-linear sm:p-8 dark:bg-gray-800'>
+                <div
+                    className={clsx(
+                        'rounded-lg bg-white p-4 shadow-md transition duration-300 ease-linear sm:p-8',
+                        'dark:bg-gray-800',
+                    )}
+                >
                     <CreateCategorieProductForm
                         className='mx-auto max-w-xl'
                         application={application}
@@ -100,11 +73,10 @@ export default function Index({ customerAuth, application, categories, localizat
                         <Droppable droppableId='categories'>
                             {(provided, snapshot) => (
                                 <ul
-                                    className={`mt-4 grid grid-cols-1 gap-4 ${
-                                        snapshot.isDraggingOver
-                                            ? 'rounded-md bg-gray-100 transition duration-300 ease-linear dark:bg-gray-900'
-                                            : ''
-                                    }`}
+                                    className={clsx('mt-4 grid grid-cols-1 gap-4', {
+                                        'rounded-md bg-gray-100 transition duration-300 ease-linear dark:bg-gray-900':
+                                            snapshot.isDraggingOver,
+                                    })}
                                     ref={provided.innerRef}
                                     {...provided.droppableProps}
                                 >
@@ -120,9 +92,17 @@ export default function Index({ customerAuth, application, categories, localizat
                                                     {...provided.draggableProps}
                                                     {...provided.dragHandleProps}
                                                     key={category.id}
-                                                    className='flex flex-col rounded-lg bg-white p-4 text-gray-900 shadow-md transition duration-300 ease-linear sm:p-8 dark:bg-gray-800 dark:text-gray-100'
+                                                    className={clsx(
+                                                        'flex flex-col rounded-lg bg-white p-4 text-gray-900 shadow-md transition duration-300 ease-linear sm:p-8',
+                                                        'dark:bg-gray-800 dark:text-gray-100',
+                                                    )}
                                                 >
-                                                    <div className='flex items-center justify-between drop-shadow-sm dark:drop-shadow-none'>
+                                                    <div
+                                                        className={clsx(
+                                                            'flex items-center justify-between drop-shadow-sm',
+                                                            'dark:drop-shadow-none',
+                                                        )}
+                                                    >
                                                         <h3>{category.name}</h3>
                                                         <div className='flex gap-2'>
                                                             <UpdateCategorieProductForm
@@ -143,7 +123,12 @@ export default function Index({ customerAuth, application, categories, localizat
                         </Droppable>
                     </DragDropContext>
                 ) : (
-                    <div className='bg-white p-4 text-center shadow transition duration-300 ease-linear sm:rounded-lg sm:p-8 dark:bg-gray-800'>
+                    <div
+                        className={clsx(
+                            'bg-white p-4 text-center shadow transition duration-300 ease-linear sm:rounded-lg sm:p-8',
+                            'dark:bg-gray-800',
+                        )}
+                    >
                         <p className='text-gray-900 dark:text-gray-100'>
                             {t('No categories of products found.')}
                         </p>
