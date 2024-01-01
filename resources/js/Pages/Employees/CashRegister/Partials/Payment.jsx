3b99ab4267fd1aea80447ca2a88c61e1e6939e6e
pieric-ux <93613004+pieric-ux@users.noreply.github.com>
2024-01-01 16:02:31 +0100

    refactor: Improve code structure and utilize clsx for class handling

--- resources/js/Pages/Employees/CashRegister/Partials/Payment.jsx
+++ resources/js/Pages/Employees/CashRegister/Partials/Payment.jsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { useState } from 'react';
 import Modal from '../../../../Components/Modal';
 import { useTranslation } from 'react-i18next';
@@ -19,14 +20,30 @@ export default function Payment({ paymentMethods, isCartEmpty, setSelectedPaymen
     return (
         <>
             <button
-                className='mt-4 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-sky-500 px-4 py-2 text-base font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-sky-700 focus:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 active:bg-sky-900 disabled:cursor-not-allowed disabled:opacity-25 dark:bg-sky-500 dark:hover:bg-sky-400 dark:focus:bg-sky-400 dark:focus:ring-offset-sky-800 dark:active:bg-sky-300'
+                className={clsx(
+                    'mt-4 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-sky-500 px-4 py-2 text-base font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out',
+                    'hover:bg-sky-700',
+                    'focus:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2',
+                    'active:bg-sky-900',
+                    'disabled:cursor-not-allowed disabled:opacity-25',
+
+                    'dark:bg-sky-500',
+                    'dark:hover:bg-sky-400',
+                    'dark:focus:bg-sky-400 dark:focus:ring-offset-sky-800',
+                    'dark:active:bg-sky-300',
+                )}
                 onClick={handleCheckout}
                 disabled={isCartEmpty}
             >
                 {t('Pay')}
             </button>
             <Modal show={openingModal} onClose={closeModal}>
-                <div className='flex flex-col gap-4 rounded-lg bg-white p-4 text-gray-900 shadow-md transition duration-300 ease-linear sm:p-8 dark:bg-gray-800 dark:text-gray-100'>
+                <div
+                    className={clsx(
+                        'flex flex-col gap-4 rounded-lg bg-white p-4 text-gray-900 shadow-md transition duration-300 ease-linear sm:p-8',
+                        'dark:bg-gray-800 dark:text-gray-100',
+                    )}
+                >
                     <div className='flex items-center justify-around gap-2'>
                         {paymentMethods.map((paymentMethod) => {
                             return (
--- resources/js/Pages/Employees/CashRegister/Partials/Total.jsx
+++ resources/js/Pages/Employees/CashRegister/Partials/Total.jsx
@@ -1,10 +1,16 @@
+import clsx from 'clsx';
 import { useTranslation } from 'react-i18next';
 
 export default function Total({ total }) {
     const { t } = useTranslation();
 
     return (
-        <div className='mt-6 flex items-center justify-center self-end rounded-lg bg-gray-100 p-4 text-gray-900 transition duration-300 ease-linear sm:p-6 dark:bg-gray-900 dark:text-gray-100'>
+        <div
+            className={clsx(
+                'mt-6 flex items-center justify-center self-end rounded-lg bg-gray-100 p-4 text-gray-900 transition duration-300 ease-linear sm:p-6',
+                'dark:bg-gray-900 dark:text-gray-100',
+            )}
+        >
             <span className='mr-2 text-xl font-medium sm:text-2xl'>{t('Total')}:</span>
             <span className='text-xl font-bold sm:text-2xl'>
                 {total} {t('currency_symbol')}
