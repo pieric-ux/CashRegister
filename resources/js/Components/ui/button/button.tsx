caffb2d612a86fbc4fb3f2d08bece292eb626f9c
pieric-ux <93613004+pieric-ux@users.noreply.github.com>
2024-01-10 13:03:27 +0100

    refactor: remove capitalize class by default

--- resources/js/Components/ui/button/button.tsx
+++ resources/js/Components/ui/button/button.tsx
@@ -4,7 +4,7 @@ import { ButtonHTMLAttributes, forwardRef } from 'react';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 const buttonVariants = cva(
-    'inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium capitalize transition duration-300 ease-linear focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+    'inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium transition duration-300 ease-linear focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
     {
         variants: {
             variant: {
