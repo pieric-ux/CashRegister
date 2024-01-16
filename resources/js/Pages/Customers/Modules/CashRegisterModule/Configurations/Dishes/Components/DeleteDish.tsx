d95810a6c488bf2da74c87cd503bdf20c0482c99
pieric-ux <93613004+pieric-ux@users.noreply.github.com>
2024-01-16 16:55:58 +0100

    move: change path for component

--- resources/js/Pages/Customers/Modules/CashRegisterModule/Configurations/Dishes/Components/DeleteDish.tsx
+++ resources/js/Pages/Customers/Modules/CashRegisterModule/Configurations/Dishes/Components/DeleteDish.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Svg } from '@/Components/ui/svg/Svg';
 import { useTranslation } from 'react-i18next';
 import { Button } from '@/Components/ui/button/button';
-import { ConfirmDeleteForm } from '@/Components/forms/ConfirmDeleteForm';
+import { ConfirmDeleteForm } from '@/Components/forms/Common/ConfirmDeleteForm';
 import {
     Dialog,
     DialogContent,
--- resources/js/Pages/Customers/Profile/Components/DeleteUser.tsx
+++ resources/js/Pages/Customers/Profile/Components/DeleteUser.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button } from '@/Components/ui/button/button';
-import { ConfirmDeleteForm } from '@/Components/forms/ConfirmDeleteForm';
+import { ConfirmDeleteForm } from '@/Components/forms/Common/ConfirmDeleteForm';
 import {
     Card,
     CardDescription,
