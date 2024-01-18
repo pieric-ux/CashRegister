dbd23f6b139e00dff463e4ba591d61fb82c608ae
pieric-ux <93613004+pieric-ux@users.noreply.github.com>
2024-01-18 17:30:04 +0100

    feat: create a GenericFormField

---
+++ resources/js/Components/forms/Common/GenericFormField.tsx
@@ -0,0 +1,43 @@
+import { Input } from '@/Components/ui/input/input';
+import { useTranslation } from 'react-i18next';
+import {
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '@/Components/ui/form/form';
+// TODO: check with Flavien how to type this
+export const GenericFormField = ({ form, setData, errors, formData }): JSX.Element => {
+    const { t } = useTranslation();
+
+    return (
+        <FormField
+            control={form.control}
+            name={formData.name}
+            render={({ field }) => (
+                <FormItem>
+                    <FormLabel>
+                        {t(formData.label)}{' '}
+                        {formData.facultative !== undefined && formData.facultative && (
+                            <small>({t('facultative')})</small>
+                        )}
+                    </FormLabel>
+                    <FormControl>
+                        <Input
+                            type={formData.type ?? 'text'}
+                            isFocused={formData.isFocused}
+                            autoComplete={formData.autoComplete}
+                            {...field}
+                            onChange={(e) => {
+                                field.onChange(e);
+                                setData(formData.name, e.target.value);
+                            }}
+                        />
+                    </FormControl>
+                    <FormMessage>{errors[formData.name]}</FormMessage>
+                </FormItem>
+            )}
+        />
+    );
+};
