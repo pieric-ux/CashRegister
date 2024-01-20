b4b97aa62f4ed24a4432497d305c0539d2b5a022
pieric-ux <93613004+pieric-ux@users.noreply.github.com>
2024-01-20 02:03:52 +0100

    refactor: restructuring import path

--- database/migrations/2023_05_22_103748_create_customers_table.php
+++ database/migrations/2023_05_22_103748_create_customers_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -18,8 +17,8 @@ return new class extends Migration
             $table->string('first_name', 45);
             $table->string('last_name', 45);
             $table->string('address');
-            $table->string('npa', 45);
             $table->string('city', 45);
+            $table->string('npa', 45);
             $table->string('phone', 45)->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
