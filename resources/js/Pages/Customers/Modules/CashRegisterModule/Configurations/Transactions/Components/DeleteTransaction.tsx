1b98becaee331c85cfae8242fbf8ab13339563b7
pieric-ux <93613004+pieric-ux@users.noreply.github.com>
2024-01-23 18:59:22 +0100

    refactor: Restructuring datas inside components

--- resources/js/Pages/Customers/Modules/CashRegisterModule/Configurations/Transactions/Components/DeleteTransaction.tsx
+++ resources/js/Pages/Customers/Modules/CashRegisterModule/Configurations/Transactions/Components/DeleteTransaction.tsx
@@ -1,25 +1,14 @@
 import { useState } from 'react';
-import { Svg } from '@/Components/ui/svg/Svg';
-import { useTranslation } from 'react-i18next';
-import { Button } from '@/Components/ui/button/button';
 import { type Transaction } from '@/Shared/Types/TransactionTypes';
+import ActionDialogButton from '@/Components/generic/ActionDialogButton';
 import { ConfirmDeleteForm } from '@/Components/forms/Auth/ConfirmDeleteForm';
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from '@/Components/ui/dialog/dialog';
+import { deleteTransactionDatas } from '@/Shared/Datas/Configs/Transactions/DeleteTransactionDatas';
 
 export default function DeleteTransaction({
     transaction,
 }: {
     transaction: Transaction;
 }): JSX.Element {
-    const { t } = useTranslation();
-
     const [open, setOpen] = useState(false);
 
     const closeDialog = (): void => {
@@ -28,35 +17,13 @@ export default function DeleteTransaction({
 
     return (
         <section>
-            <Dialog open={open} onOpenChange={setOpen}>
-                <DialogTrigger asChild>
-                    <Button
-                        variant={'destructive'}
-                        size={'icon'}
-                        aria-label={t('Delete the transaction')}
-                    >
-                        <Svg type={'delete'} variant={'destructive'} />
-                    </Button>
-                </DialogTrigger>
-                <DialogContent size={'2xl'}>
-                    <DialogHeader>
-                        <DialogTitle>
-                            {t('Are you sure you want to delete your transaction?')}
-                        </DialogTitle>
-                        <DialogDescription>
-                            {t(
-                                'Once your transaction is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your transaction.',
-                            )}
-                        </DialogDescription>
-                    </DialogHeader>
-                    <ConfirmDeleteForm
-                        route={route('transactions.destroy', transaction)}
-                        closeDialog={closeDialog}
-                        ariaLabel={t('Delete the transaction')}
-                        buttonTiltle={t('Delete Transaction')}
-                    />
-                </DialogContent>
-            </Dialog>
+            <ActionDialogButton datas={deleteTransactionDatas} open={open} setOpen={setOpen}>
+                <ConfirmDeleteForm
+                    route={route('transactions.destroy', transaction)}
+                    closeDialog={closeDialog}
+                    datas={deleteTransactionDatas}
+                />
+            </ActionDialogButton>
         </section>
     );
 }
---
+++ resources/js/Shared/Datas/Configs/Transactions/DeleteTransactionDatas.ts
@@ -0,0 +1,7 @@
+export const deleteTransactionDatas = {
+    buttonTitle: 'Delete Transaction',
+    buttonAriaLabel: 'Delete the transaction',
+    dialogTitle: 'Are you sure you want to delete your transaction?',
+    dialogDescription:
+        'Once your transaction is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your transaction.',
+};
