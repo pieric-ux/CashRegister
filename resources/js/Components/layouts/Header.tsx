3483b87a53ae5d1ef738980f92d8136e05ea8e12
pieric-ux <93613004+pieric-ux@users.noreply.github.com>
2024-01-29 03:58:20 +0100

    fix: title center in header

--- resources/js/Components/layouts/Header.tsx
+++ resources/js/Components/layouts/Header.tsx
@@ -25,7 +25,7 @@ export default function Header<T>({
 }: HeaderProps<T>): JSX.Element {
     return (
         <header className='sticky top-0 z-40 flex h-20 w-full border-b border-border bg-card drop-shadow-sm transition duration-300 ease-linear'>
-            <div className='mx-5 flex items-center sm:absolute sm:left-1/2 sm:top-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2 sm:transform'>
+            <div className='mx-5 flex items-center justify-center sm:absolute sm:left-1/2 sm:top-1/2 sm:mx-0 sm:-translate-x-1/2 sm:-translate-y-1/2 sm:transform'>
                 <h1 className='text-xl font-semibold text-foreground sm:text-2xl'>{title}</h1>
             </div>
             <div className='absolute right-5 flex h-full items-center justify-center gap-2'>
