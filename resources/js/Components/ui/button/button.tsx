4679144cd56adbfc9184f147d5e71236fbfd901a
pieric-ux <93613004+pieric-ux@users.noreply.github.com>
2024-01-29 03:35:40 +0100

    fix: button focus:outline

--- resources/js/Components/ui/button/button.tsx
+++ resources/js/Components/ui/button/button.tsx
@@ -4,19 +4,20 @@ import { forwardRef, type ButtonHTMLAttributes } from 'react';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 const buttonVariants = cva(
-    'focus-visible:ring-offset-backgrounddisabled:pointer-events-none inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium transition duration-300 ease-linear focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50',
+    'inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium transition duration-300 ease-linear focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:pointer-events-none disabled:opacity-50',
     {
         variants: {
             variant: {
-                default: 'bg-primary text-primary-foreground shadow hover:bg-primary/80',
+                default:
+                    'bg-primary text-primary-foreground shadow hover:bg-primary/80 focus-visible:ring-ring',
                 destructive:
-                    'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/80',
+                    'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/80 focus-visible:ring-destructive',
                 outline:
-                    'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
+                    'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground focus-visible:ring-ring',
                 secondary:
-                    'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary-foreground/15',
-                ghost: 'hover:bg-accent hover:text-accent-foreground',
-                link: 'text-primary underline-offset-4 hover:underline',
+                    'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary-foreground/15 focus-visible:ring-ring',
+                ghost: 'hover:bg-accent hover:text-accent-foreground focus-visible:ring-ring',
+                link: 'text-primary underline-offset-4 hover:underline focus-visible:ring-ring',
             },
             size: {
                 default: 'h-9 px-4 py-2 text-sm',
