ed05265157e9e2ce1b19ea0160f716b141dc34a2
pieric-ux <93613004+pieric-ux@users.noreply.github.com>
2024-01-30 03:03:19 +0100

    fix: title translation for EmployeeLayout

--- resources/js/Components/layouts/Header.tsx
+++ resources/js/Components/layouts/Header.tsx
@@ -5,6 +5,7 @@ import DropdownMenuLinks from './DropdownMenuLinks';
 import ThemeSwitcher from '../features/theme-switcher/ThemeSwitcher';
 import { type NavigationDatas } from '@/Shared/Types/NavigationTypes';
 import LanguageSwitcher from '../features/language-switcher/LanguageSwitcher';
+import { useTranslation } from 'react-i18next';
 
 interface HeaderProps<T> {
     user: T;
@@ -23,10 +24,11 @@ export default function Header<T>({
     drawerMenuDatas,
     slug,
 }: HeaderProps<T>): JSX.Element {
+    const { t } = useTranslation();
     return (
         <header className='sticky top-0 z-40 flex h-20 w-full border-b border-border bg-card drop-shadow-sm transition duration-300 ease-linear'>
             <div className='mx-5 flex items-center justify-center sm:absolute sm:left-1/2 sm:top-1/2 sm:mx-0 sm:-translate-x-1/2 sm:-translate-y-1/2 sm:transform'>
-                <h1 className='text-xl font-semibold text-foreground sm:text-2xl'>{title}</h1>
+                <h1 className='text-xl font-semibold text-foreground sm:text-2xl'>{t(title)}</h1>
             </div>
             <div className='absolute right-5 flex h-full items-center justify-center gap-2'>
                 <LanguageSwitcher />
