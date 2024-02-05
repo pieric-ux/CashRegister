17c8c79109dc1024eab55c33f838b34698ae18a5
pieric-ux <93613004+pieric-ux@users.noreply.github.com>
2024-02-05 16:29:18 +0100

    chore: remove old components

--- resources/js/Components/InputError.tsx
+++
@@ -1,9 +0,0 @@
-import clsx from 'clsx';
-
-export default function InputError({ message, className = '', ...props }) {
-    return message ? (
-        <p {...props} className={clsx('text-sm text-red-600 dark:text-red-400 ', className)}>
-            {message}
-        </p>
-    ) : null;
-}
