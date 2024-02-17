774bc0d404994d04fadf5a4225dd8696f71695ce
pieric-ux <93613004+pieric-ux@users.noreply.github.com>
2024-02-17 21:20:13 +0100

    fix: focus input with iOs

--- resources/js/Components/ui/input/input.tsx
+++ resources/js/Components/ui/input/input.tsx
@@ -8,7 +8,7 @@ const inputVariants = cva(
         variants: {
             variant: {
                 default:
-                    'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50',
+                    'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 shadow-sm transition placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50',
                 file: 'file:border-0 file:bg-transparent file:text-sm file:font-medium',
             },
         },
